65e490ea4baa92ed82175f6e84d183ef61fc37dd
Ricardo García Fernández <ricardogarfe@gmail.com>
2022-01-21 15:56:38 +0000

    feat: calculator requirements

--- src/main/java/com/geekshubs/calculator/Calculator.java
+++ src/main/java/com/geekshubs/calculator/Calculator.java
@@ -2,4 +2,20 @@ package com.geekshubs.calculator;
 
 public class Calculator {
 
+    public int sum(int i, int j) {
+        return i + j;
+    }
+
+    public int res(int i, int j) {
+        return i - j;
+    }
+
+    public int mul(int i, int j) {
+        return i * j;
+    }
+
+    public int div(int i, int j) {
+        return i / j;
+    }
+
 }
